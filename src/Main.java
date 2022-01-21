2c116228ef16a01db590adbf0247faf25223854b
Sticamuf <celadin@hotmail.fr>
2022-01-21 12:14:52 +0100

    fin de seance

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Window w = new Window();
+		
+		
+		
+		System.out.println("test");
 
 	}
 
--- src/Window.java
+++ src/Window.java
@@ -1,4 +1,49 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Window {
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Window extends JFrame implements ActionListener{
+
+	private JPanel containerPanel;
+	
+	private JTextField loginText;
+	private JLabel loginLabel;
+	private JLabel passwordLabel;
+	private JTextField passwordText;
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public Window() {
+		this.setSize(300,300);
+		
+		this.setTitle("loginScreen");
+		
+		containerPanel = new JPanel();
+		containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.PAGE_AXIS));
+		
+		loginLabel = new JLabel("Login : ");
+		loginText=new JTextField();
+		passwordLabel = new JLabel("Password : ");
+		passwordText = new JTextField();
+		
+		containerPanel.add(loginLabel);
+		containerPanel.add(loginText);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setContentPane(containerPanel);
+		
+		
+		this.setVisible(true);
+	}
 
 }
