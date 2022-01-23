cae109a231945bac2b0a950df34df014d32b8e2b
Adnan Mustafic <adnanmustafic00@gmail.com>
2022-01-23 18:34:28 +0100

    add visual login screen

--- src/Window.java
+++ src/Window.java
@@ -1,7 +1,11 @@
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,6 +19,9 @@ public class Window extends JFrame implements ActionListener{
 	private JLabel loginLabel;
 	private JLabel passwordLabel;
 	private JTextField passwordText;
+	private String[] lang = {"English","Français"};
+	private JComboBox<String> languageComboBox;
+	private JButton loginButton;
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -24,10 +31,17 @@ public class Window extends JFrame implements ActionListener{
 	
 	public Window() {
 		this.setSize(300,300);
+		this.setMinimumSize(new Dimension(300,300));
 		
-		this.setTitle("loginScreen");
+		this.setTitle("Safeguard");
+		
+		languageComboBox = new JComboBox<String>(lang);		
 		
 		containerPanel = new JPanel();
+		
+		languageComboBox.setMaximumSize(new Dimension(1920, 30));
+		containerPanel.add(languageComboBox);
+		containerPanel.add(Box.createRigidArea(new Dimension(0,this.getHeight()/6)));
 		containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.PAGE_AXIS));
 		
 		loginLabel = new JLabel("Login : ");
@@ -35,8 +49,22 @@ public class Window extends JFrame implements ActionListener{
 		passwordLabel = new JLabel("Password : ");
 		passwordText = new JTextField();
 		
+		loginText.setMaximumSize(new Dimension(1920, 30));
+		passwordText.setMaximumSize(new Dimension(1920, 30));
+		passwordText.setVisible(true);
+		
 		containerPanel.add(loginLabel);
 		containerPanel.add(loginText);
+		containerPanel.add(passwordLabel);
+		containerPanel.add(passwordText);
+		
+		loginButton = new JButton("Login");
+		loginButton.setMaximumSize(new Dimension(1920, 30));
+		
+		containerPanel.add(Box.createRigidArea(new Dimension(200,this.getHeight()/6)));
+		containerPanel.add(loginButton);
+		
+		
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
