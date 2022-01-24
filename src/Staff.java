5185f674ad369a5dac40cd96e85bc2ac8000c1fb
Sassy <o.sadiqi@criteo.com>
2022-01-24 21:11:29 +0100

    Adding staff save to remote db

--- src/Staff.java
+++ src/Staff.java
@@ -1,17 +1,22 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Staff {
 	
 	private String firstName;
 	private String lastName;
-	private int ID;
-	private int phoneNumber;
+	public int ID;
+	private String phoneNumber;
 	private String mail;
 	
-	public Staff(String firstName, String lastName, int iD, int phoneNumber, String mail) {
+	public Staff(String firstName, String lastName, String phoneNumber, String mail) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
-		ID = iD;
 		this.phoneNumber = phoneNumber;
 		this.mail = mail;
 	}
@@ -28,16 +33,11 @@ public class Staff {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public int getID() {
-		return ID;
-	}
-	public void setID(int iD) {
-		ID = iD;
-	}
-	public int getPhoneNumber() {
+
+	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	public void setPhoneNumber(int phoneNumber) {
+	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 	public String getMail() {
@@ -48,5 +48,36 @@ public class Staff {
 	}
 	
 	
+	public static void addStaffToDb(Staff staph) throws SQLException {
+		String sql = "INSERT INTO STAFF (first_name, last_name, phone, email)"
+				+ " values('"+staph.firstName+"', '"+staph.lastName+"', '"+staph.lastName+"', '"+staph.mail+"')";
+		SqlHandler.executeQueryWithNoReturn(sql);
+	}
+	
+	public static List<Staff> getAllStaffs() throws SQLException, ParseException{
+		String query = "select * from STAFF;";
+		List<Staff> list = new ArrayList<Staff>();
+		ResultSet result = SqlHandler.executeQuery(query);
+
+		
+		while(result.next()) {
+			Staff p = new Staff(result.getString("first_name"),
+					result.getString("last_name"),
+					result.getString("phone"),
+					result.getString("email"));
+			
+			p.ID = result.getInt("id");
+			list.add(p);
+			
+		}
+		return list;
+	}
+	
+	public static void main(String args[]) throws SQLException, ParseException{
+		SqlHandler.setTup();
+		Staff st = new Staff("Carlos", "Maria", "+336565657", "carloa@gmail.com");
+		Staff.addStaffToDb(st);
+		System.out.println(Staff.getAllStaffs());
+	}
 
 }
